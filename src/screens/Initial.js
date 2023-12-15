643e13a70e034bca0dcca83717a5d10482a2804a
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-15 17:32:00 +0530

    added lazy loading in Initial.js

--- src/screens/Initial.js
+++ src/screens/Initial.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 import TabNavigation from '../common/components/TabNavigation';
 import Dashboard from '../common/screens/Dashboard';
-import AllApps from '../common/screens/AllApps';
-import AttendanceHome from '../attendance/screens/Home';
+
+const AllApps = React.lazy(() => import('../common/screens/AllApps'));
+const AttendanceHome = React.lazy(() => import('../attendance/screens/Home'));
 
 const Stack = createNativeStackNavigator();
 const Initial = () => {
