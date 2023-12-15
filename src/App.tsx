11d5f6c5edf31c43fa15638f62de17bd381de0fc
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-12-15 12:35:39 +0530

    removed faltu code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <Text>Hi</Text>;
+}
+
+export default App;
